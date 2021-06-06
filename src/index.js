63ff88a1ebbf5990d5ec388a3a6187a3840882a2
unknown <bboinnovate20@gmail.com>
2021-06-06 22:19:51 +0100

    Change the router and some responsive component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import App from './components/App';
 import '../src/style.css'
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
-reactDOM.render(<BrowserRouter> <App /> </BrowserRouter>, document.querySelector('#root'));
+reactDOM.render(
+<React.StrictMode>
+<HashRouter> 
+<App /> 
+</HashRouter>
+</React.StrictMode>, document.querySelector('#root'));
 
 
 
